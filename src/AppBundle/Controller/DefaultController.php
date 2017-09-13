ea606b9ac00b6a149378d5c99038f158310c77fb
Marko Zovko <mazovko@foi.hr>
2017-09-13 19:11:23 +0200

    Added listing of posts publicly on /
    Changes:
    - Added post entity
    - Added fetching of all posts to DefaultController
    - Added default.html.twig template

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Post;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,9 +14,11 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        $posts = $this->getDoctrine()->getRepository(Post::class)->findAll();
+
+
+        return $this->render('@App/default.html.twig', [
+            'posts' => $posts,
         ]);
     }
 }
